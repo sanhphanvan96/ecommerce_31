75de23d8662203c2f806e77621b93e558f3d2750
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-06-09 20:00:16 +0700

    Fix regex mail,user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/home", to: "static_pages#home"
   get "/about", to: "static_pages#about"
-  get "/signup", to: "static_pages#home"
+  get "/signup", to: "users#new"
+  resources :users
 end

9adbd35a133d9c19a7beec7a6f65ff3c8bd181dd
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-06-09 20:00:16 +0700

    Fix regex mail,user

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get "/home", to: "static_pages#home"
   get "/about", to: "static_pages#about"
   get "/signup", to: "users#new"
+<<<<<<< 83d2646527dec5f49b67004e257ae960e55ae3c3
   post "/signup",  to: "users#create"
 
+=======
+>>>>>>> Fix regex mail,user
   resources :users
 end

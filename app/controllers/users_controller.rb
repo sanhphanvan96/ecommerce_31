f19b9c4e4f2344e059713ca9410e4187010e34cb
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-06-09 23:28:01 +0700

    SignUp/SignIn/SignOut

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,27 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      #@user.send_activation_email
+      #flash[:info] = "Please check your email to activate your account."
+      #redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation, :phone, :address)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get "/home", to: "static_pages#home"
   get "/about", to: "static_pages#about"
   get "/signup", to: "users#new"
+  post "/signup",  to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   resources :users
 end

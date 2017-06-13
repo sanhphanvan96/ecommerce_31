386f5320889c373fd54e1f7bfee162f067133046
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-06-14 00:05:14 +0700

    Authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "error.user.no_login"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to(root_url) unless current_user?(@user) || current_user.is_admin?
+  end
+
+  def verify_admin
+    redirect_to(root_url) unless current_user.is_admin?
+  end
 end

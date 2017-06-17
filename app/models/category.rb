9552189b0ef6abf90d202061339ac17a7b92dbee
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-06-17 11:09:53 +0700

    Category: self join category, dropdown

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :product_categories
   has_many :products, through: :product_categories
+  has_many :subcategories, :class_name => "Category", :foreign_key => :parent_id
+  has_one :parent, :class_name => "Category", :primary_key => :parent_id, :foreign_key => :id
 end

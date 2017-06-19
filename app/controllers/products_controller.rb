1978b772e12c80eb9027309be61c4464f2a053d5
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-06-19 11:46:49 +0700

    Fix search box

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,14 @@
 class ProductsController < ApplicationController
+  before_action :load_product, only: [:show, :new]
   def index
     @products = Product.all
     @products = @products.search(params[:search]) if params[:search].present?
     @products = @products.paginate(:page => params[:page],
       :per_page => Settings.max_product_per_page)
+    @categories = Category.includes(:subcategories)
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -16,4 +17,10 @@ class ProductsController < ApplicationController
 
   private
 
+  def load_product
+    @product = Product.find_by id: params[:id]
+    return if @product
+    flash[:danger] = t "error.product.not_found"
+    redirect_to root_path
+  end
 end

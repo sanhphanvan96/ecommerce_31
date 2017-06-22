4ccc85fcc44eb62e5698b56abaf63851ae7ccb63
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-06-23 01:03:37 +0700

    Fix search

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,19 +8,20 @@ class Product < ApplicationRecord
 
   scope :search_by_category, -> name, category_id {
     if name.present?
-      if (category_id == "All categories")
-        where(["name LIKE ?", "%#{name}%"])
+      if category_id.present?
+        where(["name LIKE ? AND subcategory_id LIKE ?", "%#{name}%", "%#{category_id}%"])
       else
-        where(["name LIKE ? AND subcategory_id LIKE ?", "%#{name}%", "%#{category_id}"])
+        where(["name LIKE ?", "%#{name}%"])
       end
     else
-      if (category_id == "All categories")
-        where(["name LIKE ?", "%#{name}%"])
+      if category_id.present?
+        where(["subcategory_id LIKE ?", "%#{category_id}%"])
       else
-        where(["subcategory_id LIKE ?", "%#{category_id}"])
+        where(["name LIKE ?", "%#{name}%"])
       end
     end
   }
+
   scope :order_by_created_at, ->{order created_at: :desc}
   scope :join_category, ->{
     joins(:categories).
